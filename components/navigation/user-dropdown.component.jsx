ca8efeafa979283f13fa5ccc7906f4649c0da948
Serena Dean <dserena01@yahoo.com>
2023-02-09 11:35:05 -0600

    Fix naming conventions

--- components/navigation/user-dropdown.component.jsx
+++ components/navigation/user-dropdown.component.jsx
@@ -12,7 +12,7 @@ export default function UserDropdown() {
                     <div className="w-[35px] h-[35px] shadow-primary rounded-full bg-cover inline-block bg-center h-screen bg-no-repeat" style={{"backgroundImage": `url(${PlaceholderAvatar.src})`}}></div>
                 </div>
                 A much longer name
-                <svg className="w-4 h-4 ml-2 inline" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                <svg className="w-4 h-4 ml-2 inline" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg>
             </button>
             <div className={`relative pb-3 pr-3 pl-3 leading-8 ${dropdownOpen ? "" : "hidden"}`}>
                 <ul>
