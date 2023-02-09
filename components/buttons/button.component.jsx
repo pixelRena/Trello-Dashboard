59c8ccf9c91128e4dd943cc1cc7564fb669d52e0
Serena Dean <dserena01@yahoo.com>
2023-02-09 13:05:59 -0600

    Remove padding from buttons

--- components/buttons/button.component.jsx
+++ components/buttons/button.component.jsx
@@ -1,5 +1,5 @@
 export default function Button({children}) {
     return(
-        <button className="rounded-lg text-[13px] font-normal border-white p-1 pr-3 pl-3 border-[1px] hover:bg-white hover:text-black">{children}</button>
+        <button className="rounded-lg text-[13px] font-normal border-white pr-3 pl-3 border-[1px] hover:bg-white hover:text-black">{children}</button>
     );
 }
