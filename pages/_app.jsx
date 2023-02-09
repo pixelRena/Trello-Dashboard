6a25897c519aa063573bca374fb266e7dffbcb04
Serena Dean <dserena01@yahoo.com>
2023-02-09 10:55:54 -0600

    Moved Head from index to app.jsx for reusability of title and favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import Head from "next/head";
 import Background from "@/components/background.component";
 import Sidebar from "@/components/sidebar.component";
 import Navigation from "@/components/navigation/navigation.component";
@@ -6,15 +7,23 @@ import styles from '../styles/Home.module.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={styles.main}>
-      <Background/>
-      <section>
-        <Sidebar/>
-      </section>
-      <section className="mt-5 ml-2 mr-2 lg:ml-12 lg:mr-12 w-screen">
-        <Navigation/>
-        <Component {...pageProps} />
-      </section>
-    </main>
+    <>
+      <Head>
+      <title>Trello Dashboard</title>
+      <meta name="description" content="View your trello boards statistics" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <main className={styles.main}>
+        <Background/>
+        <section>
+          <Sidebar/>
+        </section>
+        <section className="mt-5 ml-2 mr-2 lg:ml-12 lg:mr-12 w-screen">
+          <Navigation/>
+          <Component {...pageProps} />
+        </section>
+      </main>
+    </>
   )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,5 @@
-import Head from 'next/head';
+import Dashboard from "./dashboard";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Trello Dashboard</title>
-        <meta name="description" content="View your trello boards statistics" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-    </>
-  )
+  return <Dashboard/>
 }
