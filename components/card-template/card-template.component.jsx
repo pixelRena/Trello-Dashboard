ce8f9aca774d631ac529c3568e0924e962cb9077
Serena Dean <dserena01@yahoo.com>
2023-02-09 10:58:04 -0600

    Added additionalcontentclasses for 2nd div's classname

--- components/card-template/card-template.component.jsx
+++ components/card-template/card-template.component.jsx
@@ -1,8 +1,8 @@
-export default function CardTemplate ({title, width, children}) {
+export default function CardTemplate ({title, children, additionalClasses, additionalContentClasses}) {
     return(
-        <div className={`p-4 flex flex-col bg-primary-color drop-shadow-primary rounded-2xl`} style={{width: width}}>
+        <div className={`p-4 flex flex-col w-full bg-primary-color drop-shadow-primary rounded-2xl mr-5 ${additionalClasses}`}>
             <header className="text-lg pb-5">{title}</header>
-            <div className="font-extralight">{children}</div>
+            <div className={`font-extralight h-full ${additionalContentClasses}`}>{children}</div>
         </div>
     );
 }
