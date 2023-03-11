3b54ddd0d57cff15f4240082a51ec5b7593461f9
Serena Dean <dserena01@yahoo.com>
2023-03-10 18:24:47 -0600

    update exports for next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
 module.exports = {
-  nextConfig,
+  reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
