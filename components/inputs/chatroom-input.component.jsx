3e1172ea0b37bf702ab07cf202a01a47fea336c2
Serena Dean <dserena01@yahoo.com>
2023-03-14 01:53:35 -0500

    Fixed chatbox input bug

--- components/inputs/chatroom-input.component.jsx
+++ components/inputs/chatroom-input.component.jsx
@@ -1,9 +1,10 @@
 export default function ChatroomInput() {
     return(
         <div className="flex items-end h-full">
-            <div className="relative bottom-0 w-full">
-                <input type="text" className="text-sm w-full bg-transparent rounded-full border-white text-white placeholder-white focus:shadow focus:outline-none" placeholder="Type your message ..."/>
-                <div className="absolute top-2 right-2">
+            <div className="relative flex flex-row items-center bottom-0 w-full border rounded-full border-white pr-2">
+                <label className="hidden" htmlFor="messageBox">Enter your message here</label>
+                <input id="message-box" type="text" className="text-sm w-full bg-transparent border-none text-white placeholder-white focus:ring-0" placeholder="Type your message ..."/>
+                <div className="">
                     <button className="px-6 text-white rounded-full bg-active-color hover:bg-blue-700">Search</button>
                 </div>
             </div>
