d5f75dacbc527f59048f8b8e472cd2268a7eb73c
Serena Dean <dserena01@yahoo.com>
2023-03-18 17:21:39 -0500

    Testing vercel app deployment issues

--- pages/analytics.jsx
+++ pages/analytics.jsx
@@ -5,7 +5,7 @@ export default function Analytics() {
     return(
         <div className="lg:mt-0 mt-20 lg:p-0 p-5">
             <PageSubNavigation title="Analytics"/>
-            <AnalyticsCard/>
+            {/* <AnalyticsCard/> */}
         </div>
     )
 }
