4fa14621ed3d3b21bf265b838014e1dce018abfa
Serena Dean <dserena01@yahoo.com>
2023-03-18 17:45:39 -0500

    Testing vercel app deployment issues

--- pages/statistics.jsx
+++ pages/statistics.jsx
@@ -1,7 +1,7 @@
 import PageSubNavigation from "@/components/page-template/page-subnavigation.component";
 import AnalyticsCard from "@/components/analytics-items/analytics-card.component";
 
-export default function TestPageName() {
+export default function Analytics() {
     return(
         <div className="lg:mt-0 mt-20 lg:p-0 p-5">
             <PageSubNavigation title="Analytics"/>
