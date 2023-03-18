9cd83dfb51ce32866aead0886dd17f72b87cad5f
Serena Dean <dserena01@yahoo.com>
2023-03-18 17:32:14 -0500

    Testing vercel app deployment issues

--- pages/testpagename.jsx
+++ pages/testpagename.jsx
@@ -4,7 +4,7 @@ import AnalyticsCard from "@/components/analytics-items/analytics-card.component
 export default function TestPageName() {
     return(
         <div className="lg:mt-0 mt-20 lg:p-0 p-5">
-            {/* <PageSubNavigation title="Analytics"/> */}
+            <PageSubNavigation title="Analytics"/>
             {/* <AnalyticsCard/> */}
         </div>
     )
